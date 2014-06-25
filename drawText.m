152934cbb46f51634ff92fd3f6f5d8ccd9940ccd
Josh Rose <joshua.rose@rochester.edu>
2014-06-25 19:04:13 -0400

    Added Key Listener
    Detects if and when the user presses z or / for each trial

--- drawText.m
+++ drawText.m
@@ -1,11 +1,4 @@
-% DrawSomeText
-%
-% ___________________________________________________________________
-%
-%  Trivial example of drawing text.  
-% _________________________________________________________________________
-%
-% see also: PsychDemos
+% drawText
 
 % 3/8/04    awi     Wrote it.
 % 7/13/04   awi     Added comments section.  
@@ -13,10 +6,15 @@
 % 1/21/05   awi     Replaced call to GetChar with call to KbWait. 
 % 10/6/05	awi		Note here cosmetic changes by dgp between 1/21/05 and 10/6/05	.
 % 6/16/14   xxx     JOSH ROSE: Modifying the simple DrawSomeText demo for a
-% text drawing module 
+% module that draws an input string to the screen
 
 
-function dt = drawText(s)
+function [times,keys] = drawText(s)
+
+% Preallocate memory for the times array; This array stores the keypress
+% times for each trial
+times = zeros(1,length(s));
+keys = zeros(1,length(s));
 
 % Suppress Warnings
 oldEnableFlag = Screen('Preference', 'SuppressAllWarnings', 1);
@@ -24,7 +22,7 @@ oldEnableFlag = Screen('Preference', 'SuppressAllWarnings', 1);
 % Skip the graphics tests - This should be set back to default for
 % experimenting
 Screen('Preference', 'SkipSyncTests', 2 );
-fontSize = 20
+fontSize = 20;
 
 try
     % Choosing the display with the highest dislay number is
@@ -47,12 +45,19 @@ try
         
         [nx, ny, bbox] = DrawFormattedText(w, s{i}, 'center', 'center',[255, 255, 255, 255]);
         Screen('Flip',w);
-        pause(1);
+        [trialResults,press] = PressTime;
+
+        times(i) = trialResults;
+        
+        if press ~= inf
+            keys(i) = KbName(press);
+        end
+        % pause(1);
         
     end
     
-    Screen('TextFont',w, 'Times');
-    Screen('TextSize',w, 30);
+    Screen('TextFont', w, 'Times');
+    Screen('TextSize', w, 30);
     Screen('DrawText', w, 'Hit any key to exit.', 100, 300, [255, 100, 0, 255]);
     Screen('Flip',w);
     KbWait;
@@ -68,6 +73,32 @@ end
 Screen('Preference','SuppressAllWarnings',oldEnableFlag);
 Screen('Preference', 'SkipSyncTests', 0);
 
+return
 
-dt = '';
+function [pt,key] = PressTime()
+
+pt = inf;
+key = inf;
+startSecs = GetSecs;
+
+while 1
+	[ keyIsDown, timeSecs, keyCode ] = KbCheck;
+    
+    % Test to see if the word has been up for over a second; Break if yes
+    if ((GetSecs - startSecs) > 1.0)
+        break;
+    end
+    
+	if keyIsDown && (pt == Inf)
+        pt = timeSecs - startSecs;
+        key = KbName(keyCode);
+        
+        % If the user holds down a key, KbCheck will report multiple events.
+        % To condense multiple 'keyDown' events into a single event, we wait until all
+        % keys have been released.
+        while KbCheck; end
+	end
+end
+
+
 
--- main.m
+++ main.m
@@ -13,5 +13,33 @@ end
 
 % Run the experiment
 
+testWords = {'test1','test2','test3','test4'};
 
+% z = 90; / = 191z
+
+% Enable unified mode of KbName, so KbName accepts identical key names on
+% all operating systems:
+
+% KbName('UnifyKeyNames');
+
+[times,keys] = drawText(testWords);
+
+c = {'Times' 'Keys'; times keys}; 
+fid = fopen('junk.csv','w');
+
+fprintf(fid,'%s, %s\n',c{1,:});
+
+for i = 1:length(times)
+    fprintf(fid,'%f,',times(i));
+    fprintf(fid,'%f\n',keys(i));
+end
+
+
+fclose(fid);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% TODO: Convert the encodings in the keys vector to something that doesn't
+% have an upside down question mark in the output. Also, fuck varying
+% character encoding schemes
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
